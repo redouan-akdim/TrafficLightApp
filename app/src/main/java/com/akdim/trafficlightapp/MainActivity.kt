d29cc5490e5720c5c807f22ab6f5de9c9e8d1a13
Redouan <redouan.akdim@web.de>
2024-02-01 01:09:15 +0100

    feat: Added logic of traffic light

--- app/src/main/java/com/akdim/trafficlightapp/MainActivity.kt
+++ app/src/main/java/com/akdim/trafficlightapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.akdim.trafficlightapp
 
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.akdim.trafficlightapp.databinding.ActivityMainBinding
 
@@ -11,24 +13,59 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trafficLightButton: Button
     private lateinit var binding: ActivityMainBinding
 
-    // Define the paths of the traffic lights
-    private var redlight = "green_light.png"
-    private var greenlight = "green_light.png"
-    private var yellowlight = "green_light.png"
-
-    private var count = 0       // 0 -> Red, 1 -> Green, 2 -> Yellow
+    // Flags that determine the traffic light state
+    private var isGo:Boolean = false
+    private var isWait:Boolean = false
+    private var isStop:Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //val trafficLightImageView = binding.trafficLightImageView           // Image showing the traffic light
 
-        /*binding.trafficLightButton.setOnClickListener{
-            count++
-            println(binding.trafficLightImageView)
-            binding.trafficLightImageView.setImageResource(R.drawable.green_light)
-        }*/
+        // Click listener for the traffic light button
+        binding.trafficLightButton.setOnClickListener{
+            if (isStop) {
+                // Change button
+                binding.trafficLightButton.text = "Go"
+                binding.trafficLightButton.setBackgroundColor(Color.GREEN)
+
+                isStop = false
+                isGo = true
+                isWait = false
+
+                binding.greenTrafficLightImageView.visibility = View.VISIBLE
+                binding.redTrafficLightImageView.visibility = View.INVISIBLE
+                binding.yellowTrafficLightImageView.visibility = View.INVISIBLE
+            }
+            else if(isGo){
+                // Change button
+                binding.trafficLightButton.text = "Wait"
+                binding.trafficLightButton.setBackgroundColor(Color.YELLOW)
+
+                isStop = false
+                isGo = false
+                isWait = true
+
+                binding.greenTrafficLightImageView.visibility = View.INVISIBLE
+                binding.redTrafficLightImageView.visibility = View.INVISIBLE
+                binding.yellowTrafficLightImageView.visibility = View.VISIBLE
+            }
+            else{
+                // Change button
+                binding.trafficLightButton.text = "Stop"
+                binding.trafficLightButton.setBackgroundColor(Color.RED)
+
+                isStop = true
+                isGo = false
+                isWait = false
+
+                binding.greenTrafficLightImageView.visibility = View.INVISIBLE
+                binding.redTrafficLightImageView.visibility = View.VISIBLE
+                binding.yellowTrafficLightImageView.visibility = View.INVISIBLE
+            }
+        }
     }
 }
